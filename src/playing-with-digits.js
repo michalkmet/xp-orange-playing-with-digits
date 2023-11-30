6a9026493703e60949429f789bbac693c0617d54
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-11-30 09:34:45 +0100

    refactor: refactor

--- src/playing-with-digits.js
+++ src/playing-with-digits.js
@@ -4,24 +4,17 @@ function playingWithDigits(n, p) {
   }
 
   let numberArr = n.toString().split('');
-  console.log('numberArr: ', numberArr);
-
   let sumOfPows = 0;
   for (let i = 0; i < numberArr.length; i++) {
     let power = i === 0 ? p : p + i;
-    console.log('power: ', power);
-    console.log('Math.pow(numberArr[i], power): ', Math.pow(numberArr[i], power));
     sumOfPows += Math.pow(numberArr[i], power);
   }
 
-  console.log('sumOfPows: ', sumOfPows);
   if (sumOfPows < n || sumOfPows % n != 0) {
     return -1;
   } else {
-    console.log('sumOfPows / n: ', sumOfPows / n);
     return sumOfPows / n;
   }
-  // return 1;
 }
 
 module.exports = playingWithDigits;
